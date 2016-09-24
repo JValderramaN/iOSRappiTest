db01df7cae31235f015ee03c36d74e2dcd515c0e
José Angel Valderrama Noguez <josevalderrama18@gmail.com>
2016-09-24 10:36:13 -0400

    Initial Commit

---
+++ iOSRappiTestTests/iOSRappiTestTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOSRappiTestTests.swift
+//  iOSRappiTestTests
+//
+//  Created by José Valderrama on 9/24/16.
+//  Copyright © 2016 José Valderrama. All rights reserved.
+//
+
+import XCTest
+@testable import iOSRappiTest
+
+class iOSRappiTestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
